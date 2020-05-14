6ecb6f6b2cf84d30c990af0b605bce68344278d5
Chiara <Chiara.locatelli.395@gmail.com>
2020-05-14 13:03:03 +0200

    ...

--- Dispenza8/src/dispenza8/Dispenza8.java
+++ Dispenza8/src/dispenza8/Dispenza8.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package dispenza8;
+package dispensa8;
 
 /**
  *
  * @author Chiara
  */
-public class Dispenza8 {
+public class Dispensa8 {
 
     /**
      * @param args the command line arguments
